af0c0a1a65db4e66b11115ee130f1e5ef66233e9
Rafael Sales <2017infor45@gmail.com>
2023-03-01 18:49:27 -0300

    criando header e footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,88 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-     
-    </nav>
-    <router-view/>
+    <TheHeader />
+    <main>
+      <router-view />
+    </main>
+    <TheFooter />
   </div>
 </template>
 
-<style>
+<script>
+import TheHeader from '@/components/TheHeader.vue'
+import TheFooter from '@/components/TheFooter.vue'
 
+export default {
+  components: {
+    TheHeader,
+    TheFooter
+  }
+}
+</script>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  ul{
+    list-style: none;
+  }
+
+  body{
+    font-family: Arial;
+    color: #345;
+    background: url('./assets/pattern.svg') repeat top;
+  }
+
+  a{
+    text-decoration: none;
+    color: #345;
+  }
+
+  img{
+    max-width: 100%;
+    display: block;
+  }
+
+  .btn{
+    display: block;
+    padding: 10px 30px;
+    text-align: center;
+    background: #87f;
+    border-radius: 4px;
+    color: white;
+    font-size: 1rem;
+    box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+    transition: all .3s;
+    border: none;
+    font-family: Arial;
+    cursor: pointer;
+  }
+
+  .btn:hover{
+    background: #65d;
+    transform: scale(1.1);
+  }
+
+  #app{
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  main{
+    flex: 1;
+  }
+
+  body::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  body::-webkit-scrollbar-thumb {
+    background-color: rgba(136,119,255,0.5);
+    border-radius: 10px;
+  }
 </style>
