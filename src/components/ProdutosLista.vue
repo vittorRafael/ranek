0a5eb5500eac1e96bc1538b9db4a04ff0677fa3f
Rafael Sales <2017infor45@gmail.com>
2023-03-03 20:06:51 -0300

    criando view produto e o filtro para moeda

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,19 +1,22 @@
 <template>
   <section class="produtos-container">
-    <div v-if="produtos && produtos.length > 0" class="produtos">
-      <article class="produto" v-for="(produto, index) in produtos" :key="index">
-        <router-link to="/">
-          <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
-          <p class="preco">{{produto.preco}}</p>
-          <h2 class="titulo">{{produto.nome}}</h2>
-          <p>{{produto.descricao}}</p>
-        </router-link>
-      </article>
-      <ProdutosPaginar :produtosTotal="produtosTotal" :produtosPorPagina="produtosPorPagina"/>
-    </div>
-    <div v-else-if="produtos && produtos.length === 0">
-      <p class="sem-resultados">Busca sem resultados.</p>
-    </div>
+    <transition mode="out-in">
+      <div v-if="produtos && produtos.length > 0" class="produtos" key="produtos">
+        <article class="produto" v-for="(produto, index) in produtos" :key="index">
+          <router-link :to="{name: 'produto', params: {id: produto.id} }">
+            <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
+            <p class="preco">{{produto.preco | numeroPreco}}</p>
+            <h2 class="titulo">{{produto.nome}}</h2>
+            <p>{{produto.descricao}}</p>
+          </router-link>
+        </article>
+        <ProdutosPaginar :produtosTotal="produtosTotal" :produtosPorPagina="produtosPorPagina"/>
+      </div>
+      <div v-else-if="produtos && produtos.length === 0" key="sem-resultados">
+        <p class="sem-resultados">Busca sem resultados.</p>
+      </div>
+      <PaginaCarregando v-else key="carregando"/>
+    </transition>
   </section>
 </template>
 
@@ -42,11 +45,14 @@ export default {
   },
   methods: {
     getProdutos(){
-      api.get(this.url)
-        .then(r => {
-          this.produtosTotal = Number(r.headers['x-total-count'])
-          this.produtos = r.data;
-        })
+      this.produtos = null
+      setTimeout(()=>{
+        api.get(this.url)
+          .then(r => {
+            this.produtosTotal = Number(r.headers['x-total-count'])
+            this.produtos = r.data;
+          })
+      }, 1500)
     }
   },
   watch: {
